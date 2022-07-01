41c5c880ab03a21af6a93990700e6e8cdf5742e6
Matthew Murphy <MATMU@MATMU-MacBook-Pro.local>
2022-07-01 14:34:11 +0100

    Added improved content

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,20 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+router.get(/access-handler/, function (req, res) {
+    if (req.query.keys === "yes")  {
+     res.redirect('/keys-on/start');
+   } else {
+     res.redirect('/keys-off/start');
+   }
+  });
+
+  router.get(/email-handler/, function (req, res) {
+    if (req.query.email === "yes")  {
+     res.redirect('/keys-on/email');
+   } else {
+     res.redirect('/keys-on/start-date');
+   }
+  });
+
 module.exports = router;
