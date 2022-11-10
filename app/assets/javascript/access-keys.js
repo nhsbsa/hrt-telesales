af9981b4aeb2c157ed963ffc2aff7e9d09482382
George Rowley <george.rowley@nhs.net>
2022-11-10 15:29:32 +0000

    Start page access keys

--- app/assets/javascript/access-keys.js
+++ app/assets/javascript/access-keys.js
@@ -1,26 +1,49 @@
-// Start page
+// Start page access keys
+
+const certificateNumberInput = document.querySelector("[name=certificate-number]");
+const lastNameInput = document.querySelector("[name=last-name]");
+const dobDayInput = document.querySelector("[name=dob-day]");
+const postcodeInput = document.querySelector("[name=postcode]");
 
 window.addEventListener("keydown", (event) => {
-  if (event.altKey == true && event.code == "KeyT") {
-    // Access keys button
+  // Certificate number input
+  if (event.altKey === true && event.code === "KeyA") {
+    if (certificateNumberInput !== document.activeElement) {
+      certificateNumberInput.focus();
+      event.preventDefault();
+    }
+  }
+  // Last name input
+  if (event.altKey === true && event.code === "KeyZ") {
+    if (lastNameInput !== document.activeElement) {
+      lastNameInput.focus();
+      event.preventDefault();
+    }
+  }
+  // DOB day input
+  if (event.altKey === true && event.code === "KeyX") {
+    if (dobDayInput !== document.activeElement) {
+      dobDayInput.focus();
+      event.preventDefault();
+    }
+  }
+  // Postcode input
+  if (event.altKey === true && event.code === "KeyC") {
+    if (postcodeInput !== document.activeElement) {
+      postcodeInput.focus();
+      event.preventDefault();
+    }
+  }
+  // Access keys button
+  if (event.altKey === true && event.code === "KeyT") {
     document.getElementById("access-keys-button").click();
-  } else if (event.altKey == true && event.code == "KeyA") {
-    // Certificate number input
-    document.getElementById("certificate-number").focus();
-  } else if (event.altKey == true && event.code == "KeyZ") {
-    // Last name input
-    document.getElementById("last-name").focus();
-  } else if (event.altKey == true && event.code == "KeyX") {
-    // DOB input
-    document.getElementById("dob-day").focus();
-  } else if (event.altKey == true && event.code == "KeyC") {
-    // Postcode input
-    document.getElementById("postcode").focus();
-  } else if (event.altKey == true && event.code == "KeyI") {
-    // Submit button
+  }
+  // Submit keys button
+  if (event.altKey === true && event.code === "KeyI") {
     document.getElementById("submit-button").click();
-  } else if (event.altKey == true && event.code == "Key") {
-    // Cancel button
+  }
+  // Cancel button
+  if (event.altKey === true && event.code === "KeyM") {
     document.getElementById("cancel-button").click();
   }
 });
