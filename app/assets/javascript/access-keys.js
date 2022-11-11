6193b0fd5d52a6e10f441730cf5ae4965af3f601
George Rowley <george.rowley@nhs.net>
2022-11-11 10:36:00 +0000

    Customer details access keys

--- app/assets/javascript/access-keys.js
+++ app/assets/javascript/access-keys.js
@@ -52,11 +52,117 @@ window.addEventListener("keydown", (event) => {
 
 window.addEventListener("keydown", (event) => {
   // Back button
-  if (event.altKey === true && event.code === "Digit0") {
+  if (event.altKey === true && event.code === "KeyO") {
     document.getElementById("back-button").click();
   }
   // New application button
   if (event.altKey === true && event.code === "KeyY") {
     document.getElementById("new-application-button").click();
+  }
+});
+
+// Customer details
+
+const addressLineOne = document.querySelector("[name=address-line-1]");
+const addressLineTwo = document.querySelector("[name=address-line-2]");
+const townOrCity = document.querySelector("[name=address-town]");
+const appFirstName = document.querySelector("[name=app-first-name]");
+const appLastName = document.querySelector("[name=app-last-name]");
+const appDobDay = document.querySelector("[name=app-dob-day]");
+const appEmail = document.querySelector("[name=app-email]");
+const appTelephone = document.querySelector("[name=app-telephone]");
+const appNhsNumber = document.querySelector("[name=app-nhs-number]");
+const appStartDay = document.querySelector("[name=start-day]");
+const appNotes = document.querySelector("[name=app-notes]");
+
+window.addEventListener("keydown", (event) => {
+  // Address line 1
+  if (event.altKey === true && event.code === "Digit1") {
+    if (addressLineOne !== document.addressLineOne) {
+      addressLineOne.focus();
+      event.preventDefault();
+    }
+  }
+  // Address line 2
+  if (event.altKey === true && event.code === "Digit2") {
+    if (addressLineTwo !== document.activeElement) {
+      addressLineTwo.focus();
+      event.preventDefault();
+    }
+  }
+  // Town or city
+  if (event.altKey === true && event.code === "Digit3") {
+    if (townOrCity !== document.activeElement) {
+      townOrCity.focus();
+      event.preventDefault();
+    }
+  }
+  // Get address button
+  if (event.altKey === true && event.code === "KeyP") {
+    document.getElementById("get-address-button").click();
+  }
+  // Applicant first name
+  if (event.altKey === true && event.code === "KeyS") {
+    if (appFirstName !== document.activeElement) {
+      appFirstName.focus();
+      event.preventDefault();
+    }
+  }
+  // Applicant last name
+  if (event.altKey === true && event.code === "KeyA") {
+    if (appLastName !== document.activeElement) {
+      appLastName.focus();
+      event.preventDefault();
+    }
+  }
+  // Applicant DOB
+  if (event.altKey === true && event.code === "KeyX") {
+    if (appDobDay !== document.activeElement) {
+      appDobDay.focus();
+      event.preventDefault();
+    }
+  }
+  // Applicant email address
+  if (event.altKey === true && event.code === "KeyG") {
+    if (appEmail !== document.activeElement) {
+      appEmail.focus();
+      event.preventDefault();
+    }
+  }
+  // Applicant telephone number
+  if (event.altKey === true && event.code === "KeyC") {
+    if (appTelephone !== document.activeElement) {
+      appTelephone.focus();
+      event.preventDefault();
+    }
+  }
+  // Applicant NHS number
+  if (event.altKey === true && event.code === "KeyV") {
+    if (appNhsNumber !== document.activeElement) {
+      appNhsNumber.focus();
+      event.preventDefault();
+    }
+  }
+  // Certificate start date
+  if (event.altKey === true && event.code === "KeyE") {
+    if (appStartDay !== document.activeElement) {
+      appStartDay.focus();
+      event.preventDefault();
+    }
+  }
+  // Cert fulfillment email
+  if (event.altKey === true && event.code === "KeyL") {
+    document.getElementById("cert-1").click();
+  }
+  // Cert fulfillment post
+  if (event.altKey === true && event.code === "KeyN") {
+    document.getElementById("cert-2").click();
+  }
+  // Applicant notes
+  if (event.altKey === true && event.code === "KeyB") {
+    if (appNotes !== document.activeElement) {
+      appNotes.focus();
+      event.preventDefault();
+    }
   }
 });
